ff45318e3a1ffdd8c9a2f33d36ce2840f743598d
Guilherme Fünkler Borelli <borelli.guilherme@gmail.com>
2022-03-23 22:26:59 -0300

    Create Header component

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./header.css";
+import profilePicture from "../../Assets/guilherme.jpg";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <div className="info">
+        <h3>Guilherme Fünkler Borelli</h3>
+        <p>Key Account</p>
+      </div>
+      <div className="profile-picture">
+        <img src={profilePicture} alt="profilePicture" />
+      </div>
+    </header>
+  );
+}
