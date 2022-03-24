2122bfcb5a6e92a56217545dccd0e86e372f5ed8
Guilherme Fünkler Borelli <borelli.guilherme@gmail.com>
2022-03-23 22:26:03 -0300

    transform icons from svg to jsx

---
+++ src/Icons/index.js
@@ -0,0 +1,23 @@
+import Book from "./Book";
+import Camera from "./Camera";
+import ChevronDoubleUp from "./ChevronDoubleUp";
+import ChevronRight from "./ChevronRight";
+import Close from "./Close";
+import House from "./House";
+import List from "./List";
+import Pencil from "./Pencil";
+import Trash from "./Trash";
+import Question from "./Question";
+
+export {
+  Book,
+  Camera,
+  ChevronDoubleUp,
+  ChevronRight,
+  Close,
+  List,
+  House,
+  Pencil,
+  Question,
+  Trash,
+};
