b056a045c4236082facd22e00965d5aee13be7ed
Guilherme Fünkler Borelli <borelli.guilherme@gmail.com>
2022-03-23 22:28:43 -0300

    Create index.js

---
+++ src/Components/index.js
@@ -0,0 +1,8 @@
+import Breadcrumb from "./Breadcrumb/Breadcrumb";
+import Header from "./Header/Header";
+import Sidebar from "./Sidebar/Sidebar";
+import IconButton from "./IconButton/IconButton";
+import Button from "./Button/Button";
+import Label from "./Label/Label";
+
+export { Breadcrumb, Header, Sidebar, IconButton, Button, Label };
