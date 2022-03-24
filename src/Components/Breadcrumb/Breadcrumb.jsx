ce4abdbedcedd81f48af220fe7bded17b4a0da7e
Guilherme Fünkler Borelli <borelli.guilherme@gmail.com>
2022-03-23 22:27:26 -0300

    Create Breadcrumb component

---
+++ src/Components/Breadcrumb/Breadcrumb.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./breadcrumb.css";
+import { ChevronRight } from "../../Icons";
+export default function Breadcrumb() {
+  return (
+    <nav className="breadcrumb">
+      <Link to={"/"}>Home</Link>
+      <ChevronRight />
+      <Link to={"courses-list"}>Treinamento</Link>
+    </nav>
+  );
+}
