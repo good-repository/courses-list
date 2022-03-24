af86ce9a69fc2e8a0f009632cdd84e1179a1cf94
Guilherme Fünkler Borelli <borelli.guilherme@gmail.com>
2022-03-23 22:29:04 -0300

    Create Sidebar component

---
+++ src/Components/Sidebar/Sidebar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Book, House, List } from "../../Icons";
+import IconButton from "../IconButton/IconButton";
+import "./sidebar.css";
+
+export default function Sidebar() {
+  return (
+    <nav className="sidebar">
+      <div className="menu">
+        <IconButton icon={<List />} />
+      </div>
+      <div className="home">
+        <Link to={"/"}>
+          <IconButton icon={<House />} />
+        </Link>
+      </div>
+      <div className="courses">
+        <Link to={"/courses-list"}>
+          <IconButton icon={<Book />} />
+        </Link>
+      </div>
+    </nav>
+  );
+}
