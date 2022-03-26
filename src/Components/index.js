b4e6538f12a6a9ae1a12b22e1fa4aaa4c2be5fa8
Guilherme Fünkler Borelli <borelli.guilherme@gmail.com>
2022-03-26 03:03:46 -0300

    Update index.js

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,7 +1,10 @@
 import Breadcrumb from "./Breadcrumb/Breadcrumb";
 import Button from "./Button/Button";
+import DateInput from "./DateInput/DateInput";
 import Header from "./Header/Header";
 import IconButton from "./IconButton/IconButton";
+import ImageUploader from "./ImageUploader/ImageUploader";
+import Input from "./Input/Input";
 import Label from "./Label/Label";
 import Sidebar from "./Sidebar/Sidebar";
 import SlidableSidebar from "./SlidableSidebar/SlidableSidebar";
@@ -9,8 +12,11 @@ import SlidableSidebar from "./SlidableSidebar/SlidableSidebar";
 export {
   Breadcrumb,
   Button,
+  DateInput,
   Header,
   IconButton,
+  ImageUploader,
+  Input,
   Label,
   Sidebar,
   SlidableSidebar,
