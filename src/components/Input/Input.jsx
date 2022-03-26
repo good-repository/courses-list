c8d81e3b5cb6f6ad93403358bae7ec7a25885334
Guilherme Fünkler Borelli <borelli.guilherme@gmail.com>
2022-03-26 11:50:47 -0300

    update Input

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,26 +1,13 @@
 import React from "react";
 import "./input.css";
 
-export default function Input({
-  type = "text",
-  label,
-  size = "default",
-  id,
-  name,
-  onChange,
-}) {
+export default function Input({ type = "text", label, id, name, onChange }) {
   return (
     <div className="input">
       <label htmlFor={id} className="input-label">
         {label}
       </label>
-      <input
-        type={type}
-        id={id}
-        className={`input-${size}`}
-        name={name}
-        onChange={onChange}
-      />
+      <input type={type} id={id} name={name} onChange={onChange} />
     </div>
   );
 }
