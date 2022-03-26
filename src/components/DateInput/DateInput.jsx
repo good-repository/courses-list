ece594688c5a58d015aa9caf418c5469f8eba232
Guilherme Fünkler Borelli <borelli.guilherme@gmail.com>
2022-03-26 14:02:34 -0300

    update DateInput

--- src/components/DateInput/DateInput.jsx
+++ src/components/DateInput/DateInput.jsx
@@ -1,14 +1,7 @@
 import React from "react";
 import "./dateInput.css";
 
-export default function DateInput({
-  id,
-  label,
-  size = "default",
-  minDate,
-  name,
-  onChange,
-}) {
+export default function DateInput({ id, label, minDate, name, onChange }) {
   return (
     <div className="date-input">
       <label htmlFor={id} className="date-input-label">
@@ -18,7 +11,6 @@ export default function DateInput({
         type="date"
         id={id}
         min={minDate}
-        className={`date-input-${size}`}
         name={name}
         onChange={onChange}
       />
