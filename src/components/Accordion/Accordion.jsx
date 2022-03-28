d64c7d98a26e44913cf9bc586e0f1987bef2dca5
Guilherme Fünkler Borelli <borelli.guilherme@gmail.com>
2022-03-27 22:25:05 -0300

    Create Accordion component

---
+++ src/components/Accordion/Accordion.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import { ChevronDoubleUp } from "../../icons";
+import "./accordion.css";
+
+export default function Accordion({ title, label, children }) {
+  const [show, setShow] = useState(false);
+
+  return (
+    <div className="accordion-wrapper">
+      <button
+        className={`accordion-button ${show ? "open-accordion" : ""}`}
+        onClick={() => setShow(!show)}
+      >
+        <p className="accordion-title">
+          {title}
+          {label}
+        </p>
+        <div className={show ? "" : "chevron-down"}>
+          <ChevronDoubleUp />
+        </div>
+      </button>
+      {show && <div className="accordion-content animated">{children}</div>}
+    </div>
+  );
+}
