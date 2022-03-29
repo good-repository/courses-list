8e487d2dbfab7cde6e3c882117bc1c546cd3a64e
Guilherme Fünkler Borelli <borelli.guilherme@gmail.com>
2022-03-29 19:42:06 -0300

    improvements css

--- src/components/Breadcrumb/Breadcrumb.jsx
+++ src/components/Breadcrumb/Breadcrumb.jsx
@@ -1,13 +1,18 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./breadcrumb.css";
 import { ChevronRight } from "../../icons";
 export default function Breadcrumb() {
+  const location = useLocation();
   return (
     <nav className="breadcrumb">
       <Link to={"/"}>Home</Link>
-      <ChevronRight />
-      <Link to={"courses-list"}>Treinamento</Link>
+      {location.pathname.includes("/courses-list") && (
+        <div className="breadcrumb-link">
+          <ChevronRight />
+          <Link to={"courses-list"}>Treinamento</Link>
+        </div>
+      )}
     </nav>
   );
 }
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,23 +1,52 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 import { Book, House, List } from "../../icons";
 import IconButton from "../IconButton/IconButton";
 import "./sidebar.css";
 
 export default function Sidebar() {
+  const [expanded, setExpanded] = useState(true);
+  const location = useLocation();
+
   return (
-    <nav className="sidebar">
+    <nav className={`sidebar ${expanded ? "expanded" : ""}`}>
       <div className="menu">
-        <IconButton icon={<List />} />
+        <div className="menu-icon">
+          <IconButton
+            className="sidebar-link"
+            icon={<List />}
+            onClick={() => setExpanded(!expanded)}
+          />
+        </div>
       </div>
-      <div className="home">
-        <Link to={"/"}>
+      <div
+        className={`home ${
+          location.pathname === "/" ? "active-route" : "inactive-route"
+        } `}
+      >
+        <Link
+          className="sidebar-link"
+          to={"/"}
+          onClick={() => setExpanded(false)}
+        >
           <IconButton icon={<House />} />
+          <p>Início</p>
         </Link>
       </div>
-      <div className="courses">
-        <Link to={"/courses-list"}>
+      <div
+        className={`courses ${
+          location.pathname.includes("/courses-list")
+            ? "active-route"
+            : "inactive-route"
+        } `}
+      >
+        <Link
+          className="sidebar-link"
+          to={"/courses-list"}
+          onClick={() => setExpanded(false)}
+        >
           <IconButton icon={<Book />} />
+          <p>Treinamento</p>
         </Link>
       </div>
     </nav>
